f00ec918b92381c453c2a9c6a8dc2d5a5fe90ffc
Rafael Fernandes <spnrafael@gmail.com>
2019-10-13 20:07:49 -0300

    Courses new

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,9 @@
 class Course < ApplicationRecord
+  has_rich_text :about
+
+  validates :name, presence: true
+  validates :about, presence: true
+  validates :price, presence: true
+  validates :duration, presence: true
+  validates :when, presence: true
 end
